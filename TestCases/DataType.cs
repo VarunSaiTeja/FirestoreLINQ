c70218997b6dc353639463f86f1062048587bdbd
Varun Teja <35555010+VarunSaiTeja@users.noreply.github.com>
2022-02-15 09:05:03 +0530

    data type- few  test cases added

---
+++ TestCases/DataType.cs
@@ -0,0 +1,35 @@
+﻿using Google.Cloud.Firestore;
+using Google.Type;
+using System;
+
+namespace TestCases
+{
+    [FirestoreData]
+    public class DataType : IEquatable<DataType>
+    {
+        [FirestoreProperty]
+        public double Double { get; set; }
+
+        [FirestoreProperty]
+        public float Float { get; set; }
+
+        [FirestoreProperty]
+        public string String { get; set; }
+
+        [FirestoreProperty]
+        public LatLng LatLng { get; set; }
+
+        [FirestoreProperty]
+        public bool Bool { get; set; }
+
+        public bool Equals(DataType other)
+        {
+            var floatingPoints = other.Float == Float && other.Double == Double;
+            var text = other.String == String;
+            var point = other.LatLng.Latitude == LatLng.Latitude && other.LatLng.Longitude == LatLng.Longitude;
+            var boolean = other.Bool == Bool;
+
+            return floatingPoints && text && point && boolean;
+        }
+    }
+}
---
+++ TestCases/DataTypeCases.cs
@@ -0,0 +1,41 @@
+﻿using FirestoreLINQ;
+using System.Linq;
+using Xunit;
+
+namespace TestCases
+{
+    public class DataTypeCases : IClassFixture<MyDb>
+    {
+        private readonly MyDb _db;
+
+        private IQueryable<DataType> types => _db.firestoreDb.Collection("Types").AsQuerable<DataType>();
+
+        public DataTypeCases(MyDb db)
+        {
+            _db = db;
+        }
+
+        DataType dataTypes = new()
+        {
+            Double = 77578.55,
+            Float = 1.056f,
+            String = "Hey",
+            LatLng = new Google.Type.LatLng() { Latitude = -45, Longitude = 120 },
+            Bool = true
+        };
+
+        [Fact]
+        public void MockData()
+        {
+            var rs = _db.firestoreDb.Collection("Types").Document("Test").SetAsync(dataTypes).Result;
+            Assert.NotNull(rs);
+        }
+
+        [Fact]
+        public void GetEntireType()
+        {
+            var result = types.First();
+            Assert.Equal(dataTypes, result);
+        }
+    }
+}
